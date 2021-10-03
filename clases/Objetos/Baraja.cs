456f5fc4e9aea7250df529a29cbb0d64a58555fc
santiago jagua <santiago.jagua.2021@upb.edu.co>
2021-10-03 16:10:08 -0500

    Agregar archivos de proyecto.

---
+++ clases/Objetos/Baraja.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases.Objetos
+{
+    class Baraja
+    {
+        Queue<Carta> MazoCartas;
+
+        List<int> Cartasint;
+
+        Random generador = new Random();
+
+        private void GenerarCartasint()
+        {
+            Cartasint = new List<int>();
+            for (int i = 0; i < 52; i++)
+            {
+                Cartasint.Add(i);
+            }
+        }
+
+        public void Aleatorio()
+        {
+            MazoCartas = new Queue<Carta>();
+            GenerarCartasint();
+            for (int i = 51; i >= 0; i--)
+            {
+                int index = generador.Next(0, i);
+                int temp = Cartasint[i];
+                Cartasint[i] = Cartasint[index];
+                Cartasint[index] = temp;
+            }
+            Barajajuego();
+        }
+
+        private void Barajajuego()
+        {
+            for (int i = 0; i < Cartasint.Count; i++)
+            {
+                PintaCarta pinta = (PintaCarta)(Cartasint[i] % 4);
+                ValorCarta valor = (ValorCarta)(Cartasint[i] % 13 + 1);
+                MazoCartas.Enqueue(new Carta(pinta, valor));
+            }
+        }
+
+        public Carta CartasCrupier()
+        {
+            return MazoCartas.Dequeue();
+        }
+    }
+}
---
+++ clases/Objetos/Carta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clases.Objetos
+{
+    public struct Carta
+    {
+        private PintaCarta pinta;
+        private ValorCarta valor;
+        public PintaCarta Suit
+        {
+            get
+            {
+                return this.pinta;
+            }
+        }
+        public ValorCarta Value
+        {
+            get
+            {
+                return this.valor;
+            }
+        }
+        public Carta(PintaCarta pinta, ValorCarta valor)
+            : this()
+        {
+            this.pinta = pinta;
+            this.valor = valor;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Pinta: {0}, Value: {1}", this.pinta, this.valor);
+        }
+    }
+}
